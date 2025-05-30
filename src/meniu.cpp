f11bea7851c48bf466f1a4dd3633b20edd6d7ff2
Rokinis <147270094+Rokuts@users.noreply.github.com>
2025-05-31 02:09:03 +0300

    itrauktas meniu

--- src/meniu.cpp
+++ src/meniu.cpp
@@ -13,4 +13,5 @@ void meniu()
     cout << "8. Suskirstyti studentus i vargsiukus, kietiakus ir isvesti juos i du skirtingus failus" <<endl;
     cout << "9. Laiko tyrimas" << endl;
     cout << "10. Tyrimas su strategijom" << endl;
+    cout << "11. Klases testavimas" << endl;
 }
