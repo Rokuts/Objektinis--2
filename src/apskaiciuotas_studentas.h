5788e2d3d38c07c13057f97402831326dac86509
Rokinis <147270094+Rokuts@users.noreply.github.com>
2025-05-31 18:38:02 +0300

    apskaiciuotas studentas paveldi studento klase

--- src/apskaiciuotas_studentas.h
+++ src/apskaiciuotas_studentas.h
@@ -3,24 +3,19 @@
 #include "headers.h"
 #include "studentas.h"
 
-struct ApskaiciuotasStudentas{
+class ApskaiciuotasStudentas: public Studentas{
 private:
-    Studentas studentas_;
     double vidurkis_;
     double mediana_;
 
 public:
     //Konstruktorius
     ApskaiciuotasStudentas(const Studentas& stud)
-        : studentas_(stud), vidurkis_(stud.vidurkis()), mediana_(stud.mediana()) {}
+        : Studentas(stud), vidurkis_(stud.vidurkis()), mediana_(stud.mediana()) {}
 
-    inline Studentas getStudentas() const { return studentas_; }
     inline double getVidurkis() const { return vidurkis_; }
     inline double getMediana() const { return mediana_; }
 
-    inline string getVardas() const { return studentas_.getVardas(); }
-    inline string getPavarde() const { return studentas_.getPavarde(); }
-    inline int getEgzas() const { return studentas_.getEgzas(); }
 };
 
 
