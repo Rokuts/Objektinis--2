8d93341480c2850dfc82d8dcc4bb77b50f5c5fbc
Rokinis <147270094+Rokuts@users.noreply.github.com>
2025-05-31 20:44:17 +0300

    pildomas vektorius

--- src/vektorius.cpp
+++ src/vektorius.cpp
@@ -4,3 +4,40 @@ template <typename T>
 inline Vektorius<T>::Vektorius()
 {
 }
+
+template <typename T>
+Vektorius<T>::Vektorius(size_t n, const T &value)
+{
+}
+
+template <typename T>
+Vektorius<T>::Vektorius(const Vektorius &other)
+{
+}
+
+template <typename T>
+Vektorius<T>::Vektorius(Vektorius &&other) noexcept
+{
+}
+
+template <typename T>
+Vektorius<T> &Vektorius<T>::operator=(const Vektorius<T> &other)
+{
+    // TODO: insert return statement here
+}
+
+template <typename T>
+Vektorius<T> &Vektorius<T>::operator=(Vektorius<T> &&other) noexcept
+{
+    // TODO: insert return statement here
+}
+
+template <typename T>
+Vektorius<T>::~Vektorius()
+{
+}
+
+template <typename T>
+void Vektorius<T>::push_back(const T &value)
+{
+}
